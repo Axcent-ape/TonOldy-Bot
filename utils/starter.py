7b2e244a3317cd6458a5244a1b9f71e70e45b359
Axcent <97802272+Axcent-ape@users.noreply.github.com>
2024-09-25 21:19:02 +0300

    Add files via upload

---
+++ utils/starter.py
@@ -0,0 +1,53 @@
+import random
+from utils.ton_oldy import TonOldy
+from data import config
+from utils.core import logger
+import datetime
+import pandas as pd
+from utils.core.telegram import Accounts
+import asyncio
+import os
+
+
+async def start(thread: int, session_name: str, phone_number: str, proxy: [str, None]):
+    oldy = TonOldy(session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)
+    account = session_name + '.session'
+
+    while True:
+        await oldy.login()
+
+        info = await oldy.get_challenge()
+        if not info.get('dailyHuntIsCompleted'):
+            word = oldy.generate_word(info.get('dailyHuntWordHash'), info.get('dailyHuntWordLength'))
+
+            if await oldy.submit_daily_hunts(word):
+                info = await oldy.get_challenge()
+                logger.success(f"Thread {thread} | {account} | Submitted daily hunt word «{info.get('dailyHuntWordCompleted')}» and got {info.get('dailyHuntCurrentReward')} STONE")
+            else:
+                logger.warning(f"Thread {thread} | {account} | Submitted incorrect daily hunt word «{word}»")
+
+        await oldy.logout()
+
+        sleep = oldy.get_sleep_time() + random.uniform(*config.DELAYS['ADDITION_SLEEP'])
+        logger.info(f"Thread {thread} | {account} | Sleep {int(sleep)}")
+        await asyncio.sleep(sleep)
+
+    await oldy.logout()
+
+async def stats():
+    accounts = await Accounts().get_accounts()
+
+    tasks = []
+    for thread, account in enumerate(accounts):
+        session_name, phone_number, proxy = account.values()
+        tasks.append(asyncio.create_task(TonOldy(session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy).stats()))
+
+    data = await asyncio.gather(*tasks)
+    path = f"statistics/statistics_{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.csv"
+    columns = ['Phone number', 'Name', 'balance', 'leaderboard', 'referrals', 'Proxy (login:password@ip:port)']
+
+    if not os.path.exists('statistics'): os.mkdir('statistics')
+    df = pd.DataFrame(data, columns=columns)
+    df.to_csv(path, index=False, encoding='utf-8-sig')
+
+    logger.success(f"Saved statistics to {path}")
